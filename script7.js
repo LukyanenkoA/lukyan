f2384c123c97ff45f0410ab3b1496e755136eedd
LukyanenkoA <112947651+LukyanenkoA@users.noreply.github.com>
2022-10-23 18:51:07 +0300

    Add files via upload

---
+++ script7.js
@@ -0,0 +1,42 @@
+"use strict"
+$(document).ready(function () {
+
+	$(".slider").slick({	
+		dots: true,
+		infinite: true,
+		
+		speed: 200,
+		
+		slidesToShow: 4,
+		
+		slidesToScroll: 4,
+		
+		prevArrow: $('#prev'),
+		
+		nextArrow: $('#next'),
+		
+		responsive: [
+			{
+				
+			    breakpoint: 768,
+			
+			    settings: {
+				
+		            slidesToShow: 2,
+					
+                            slidesToScroll: 2,
+					
+                            infinite: true,
+					
+                            dots: true
+				
+                         }
+			
+                    }
+
+                 ]
+	
+	});
+
+});
+ 
